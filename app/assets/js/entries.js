535d24ea6e7c40f6a8b95b37602d91ad34984f9d
patriciagisla-sys <patriciagisla@gmail.com>
2026-01-27 14:58:11 +0100

    Update entries.js

--- app/assets/js/entries.js
+++ app/assets/js/entries.js
@@ -5,17 +5,25 @@ import {
   serverTimestamp
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-export async function saveExercise(type, data) {
+export async function saveExercise(exerciseKey, data) {
   const user = auth.currentUser;
 
   if (!user) {
     throw new Error("Usuario no autenticado");
   }
 
-  return await addDoc(collection(db, "entries"), {
-    uid: user.uid,
-    type,
-    data,
-    createdAt: serverTimestamp()
-  });
+  return await addDoc(
+    collection(
+      db,
+      "users",
+      user.uid,
+      "exercises",
+      exerciseKey,
+      "answers"
+    ),
+    {
+      data,
+      createdAt: serverTimestamp()
+    }
+  );
 }
