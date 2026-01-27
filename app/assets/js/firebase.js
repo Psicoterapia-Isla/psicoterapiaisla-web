2d195cd0ab6df546073e8b0aa5cf2a0ee96a8895
patriciagisla-sys <patriciagisla@gmail.com>
2026-01-27 13:55:57 +0100

    Update firebase.js

--- app/assets/js/firebase.js
+++ app/assets/js/firebase.js
@@ -3,12 +3,13 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth
 import { getFirestore } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDfxdzL39Ne4XdT9WgSLz3iSliyg-xBR84",
+  apiKey: "AIzaSyDfxdzl39Ne4XdT9WgSlz3iSliyg-xBR84",
   authDomain: "psicoterapia-isla-app.firebaseapp.com",
   projectId: "psicoterapia-isla-app",
-  storageBucket: "psicoterapia-isla-app.appspot.com",
+  storageBucket: "psicoterapia-isla-app.firebasestorage.app",
   messagingSenderId: "824485435208",
-  appId: "1:824485435208:web:79d2a122d975e2b5cf857d"
+  appId: "1:824485435208:web:79d2a122d975e2b5cf857d",
+  measurementId: "G-9W5Y8BP8DE"
 };
 
 export const app = initializeApp(firebaseConfig);
