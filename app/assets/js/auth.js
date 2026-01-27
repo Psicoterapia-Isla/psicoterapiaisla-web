f95acbe6e79045826387a6c1ca09a2e2341b45c5
patriciagisla-sys <patriciagisla@gmail.com>
2026-01-27 14:21:06 +0100

    Update auth.js

--- app/assets/js/auth.js
+++ app/assets/js/auth.js
@@ -1,40 +1,19 @@
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+// app/assets/js/auth.js
+
+import { auth } from "./firebase.js";
 import {
-  getAuth,
   signInWithEmailAndPassword,
-  onAuthStateChanged,
-  signOut
+  onAuthStateChanged
 } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-auth.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDfxdzL39Ne4XdT9WgSLz3iSliyg-xBR84",
-  authDomain: "psicoterapia-isla-app.firebaseapp.com",
-  projectId: "psicoterapia-isla-app",
-  storageBucket: "psicoterapia-isla-app.firebasestorage.app",
-  messagingSenderId: "824485435208",
-  appId: "1:824485435208:web:79d2a122d975e2b5cf857d",
-  measurementId: "G-9W5Y8BP8DE"
-};
-
-const app = initializeApp(firebaseConfig);
-export const auth = getAuth(app);
-
-// LOGIN
 export async function login(email, password) {
   return await signInWithEmailAndPassword(auth, email, password);
 }
 
-// LOGOUT
-export async function logout() {
-  await signOut(auth);
-  window.location.href = "login.html";
-}
-
-// PROTECCIÓN DE PÁGINAS
 export function requireAuth() {
   onAuthStateChanged(auth, (user) => {
     if (!user) {
-      window.location.href = "login.html";
+      window.location.href = "/app/login.html";
     }
   });
 }
