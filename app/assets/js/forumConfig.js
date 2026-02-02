23da97614cdcce5aeca4d449b453a1c73a39955e
patriciagisla-sys <patriciagisla@gmail.com>
2026-02-02 15:10:41 +0100

    Update forumConfig.js

--- app/assets/js/forumConfig.js
+++ app/assets/js/forumConfig.js
@@ -1,4 +1,4 @@
-// forumConfig.js
-
 export const FORUMS_COLLECTION = "forums";
 export const FORUM_ID = "aGuX3GfOqrglDg5cElpv";
+export const TOPICS_COLLECTION = "topics";
+export const POSTS_COLLECTION = "posts";
